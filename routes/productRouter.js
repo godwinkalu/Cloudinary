a125d0687d41991798eb4666aad2e3c35e3946c7
godwinkalu <kalugodwin158@gmail.com>
2025-09-20 10:43:26 +0100

    cloudinary updated

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const upload = require('../middleware/multer')
-const { addProduct,  getOneProduct, getAllProduct, deleteProduct } = require('../controller/productController')
+const { addProduct,  getOneProduct, getAllProduct, deleteProduct, updateProduct } = require('../controller/productController')
 router.post('/product', upload.array('image',5), addProduct)
 router.get('/product', getAllProduct)
 router.get('/product/:id', getOneProduct)
-router.put('/product/:id', upload.array('image',5), )
+router.put('/product/:id', upload.array('image',5), updateProduct)
 router.delete('/product/:id', deleteProduct)
 
 
